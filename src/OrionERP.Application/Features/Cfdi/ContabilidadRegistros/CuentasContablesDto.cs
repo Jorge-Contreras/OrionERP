c00c24c4b512353757a53b8910742eb8cafa3eec
Jorge-Contreras <jc_carpio@hotmail.com>
2025-12-18 14:19:05 -0600

    Update cuentas contables RFC column usage

--- src/OrionERP.Application/Features/Cfdi/ContabilidadRegistros/CuentasContablesDto.cs
+++ src/OrionERP.Application/Features/Cfdi/ContabilidadRegistros/CuentasContablesDto.cs
@@ -3,7 +3,7 @@ namespace OrionERP.Application.Features.Cfdi.ContabilidadRegistros;
 public sealed record CuentasContablesDto
 {
   public int Id { get; init; }
-  public string RazonSocial { get; init; } = string.Empty;
+  public string Rfc { get; init; } = string.Empty;
   public string Nivel1 { get; init; } = string.Empty;
   public string Nivel2 { get; init; } = string.Empty;
   public string Nivel3 { get; set; } = string.Empty;
